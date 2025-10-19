bfee8b3e52a3a8a61ae483250c31a22dc5ad5305
cderamos-2ct <cderamos@2ct.com>
2025-10-19 08:31:23 -0700

    Fix: Copy node icons to individual node directories for proper display

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,30 @@ function copyIcons() {
     .pipe(dest('dist'));
 }
 
+function copyNodeIcons() {
+  return src('nodes/MightyCall/*.svg')
+    .pipe(dest('dist/nodes/MightyCall'));
+}
+
+function copyOnPrintShopIcons() {
+  return src('nodes/OnPrintShop/*.svg')
+    .pipe(dest('dist/nodes/OnPrintShop'));
+}
+
+function copyXPSShipIcons() {
+  return src('nodes/XPSShip/*.svg')
+    .pipe(dest('dist/nodes/XPSShip'));
+}
+
 function copyCredentialIcons() {
   return src('credentials/*.svg')
     .pipe(dest('dist/credentials'));
 }
 
 exports['build:icons'] = copyIcons;
+exports['build:node-icons'] = copyNodeIcons;
+exports['build:onprintshop-icons'] = copyOnPrintShopIcons;
+exports['build:xps-icons'] = copyXPSShipIcons;
 exports['build:credential-icons'] = copyCredentialIcons;
 exports.default = copyIcons;
 
